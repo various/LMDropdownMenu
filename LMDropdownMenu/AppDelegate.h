83e3f55b9df8863028d2a2b6fda16a96ccf3ba24
Various <apple123@mail.ustc.edu.cn>
2015-04-03 16:39:49 +0800

    init

---
+++ LMDropdownMenu/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LMDropdownMenu
+//
+//  Created by Tim Geng on 4/3/15.
+//  Copyright (c) 2015 GF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
